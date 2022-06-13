77b11d78dee9974971c3315315ec8b3c06072616
YelloElefant <89622961+YelloElefant@users.noreply.github.com>
2022-06-14 00:24:03 +1200

    more impvements

--- assesmentWpf/assesmentWpf/MainWindow.xaml.cs
+++ assesmentWpf/assesmentWpf/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace assesmentWpf
 
         public Player()
         {
-            this.visual.Source = new BitmapImage(new Uri("K:/12AI/AssesmentDev/Sprites/player.gif"));
+            this.visual.Source = new BitmapImage(new Uri("C:/Users/YelloElefant/OneDrive/Coding/visual studio/Assesment/Sprites/player.gif"));
             this.visual.Width = 50;
             this.visual.Height = 50;
             this.visual.Stretch = Stretch.Fill;
@@ -49,7 +49,6 @@ namespace assesmentWpf
 
     public partial class MainWindow : Window
     {
-        public double wHeight, wWidth;
         Player player = new Player();
         public MainWindow()
         {
@@ -68,16 +67,17 @@ namespace assesmentWpf
 
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
-            
-            
 
 
-            var pos = GetMousePos(player.visual);
+
+
+            var pos = Mouse.GetPosition(player.visual);
+            pos.Offset(player.visual.Width / 2, player.visual.Height / 2);
             var angle = GetAngle(pos);
 
             lbl_output.Content = angle.ToString();
             lbl_output_pos.Content = pos.ToString();
-            RotateTransform rotateTransform = new RotateTransform(angle, 50, 50);
+            RotateTransform rotateTransform = new RotateTransform(angle, 25, 25);
             player.visual.RenderTransform = rotateTransform;
             //lbl_output.Content = GetMousePos(player); 
         }
@@ -86,25 +86,25 @@ namespace assesmentWpf
         {
             if (mouse.X > 0 && mouse.Y > 0) //bottom right
             {
-                return (Math.Atan(mouse.Y / mouse.X)) * (180 / Math.PI);
+                return (Math.Atan(mouse.Y / mouse.X)) * ( Math.PI);
             }
             else if (mouse.X < 0 && mouse.Y > 0) //bottom left
             {
                 mouse.X *= -1;
-                var ang = Math.Atan(mouse.Y / mouse.X) * (180 / Math.PI);
+                var ang = Math.Atan(mouse.Y / mouse.X) * ( Math.PI);
                 return 90 + (90 - ang);
             }
             else if (mouse.X > 0 && mouse.Y < 0) //top right
             {
                 mouse.Y *= -1;
-                var ang = Math.Atan(mouse.Y / mouse.X) * (180 / Math.PI);
+                var ang = Math.Atan(mouse.Y / mouse.X) * ( Math.PI);
                 return 360 - ang;
             }
             else if (mouse.X < 0 && mouse.Y < 0) //top left
             {
                 mouse.X *= -1;
                 mouse.Y *= -1;
-                var ang = Math.Atan(mouse.Y / mouse.X) * (180 / Math.PI);
+                var ang = Math.Atan(mouse.Y / mouse.X) * ( Math.PI);
                 return 180 + ang;
             }
             return -1;
@@ -114,15 +114,7 @@ namespace assesmentWpf
 
         }
 
-        static Point GetMousePos(Image p)
-        {
-
-            Point mousePos = Mouse.GetPosition(p);
-           
-
-            
-            return mousePos;
-        }
+        
 
 
         private void btn_look_KeyDown(object sender, KeyEventArgs e)
